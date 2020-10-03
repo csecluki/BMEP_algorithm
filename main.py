d7a985e729748c9ed61facbceaaf66d020bb8369
csecluki <lukaszzagorski@interia.pl>
2020-10-03 22:28:34 +0200

    Changed data source - filepath is now given as an argument

--- main.py
+++ main.py
@@ -1,84 +1,103 @@
 from tabulate import tabulate
+import argparse
+import os
 
-roads = []
-iteration = 0
-queue = []
-headers = ["Iteration", "Queue"]
-tot = 0
-table = []
 
-starting_point = input("Podaj punkt startowy: ")
-ending_point = input("Podaj punkt koncowy: ")
+def main(filename):
+    headers = ["Iteration", "Queue"]
+    table = []
+    roads = []
+    queue = []
+    tot = 0
+    iteration = 0
 
-headers.append(starting_point)
-
-while True:
-    new_record = input("Podaj trase (start_koniec_dlugosc): ")
-    if new_record != "":
-        new_record = new_record.split(sep=" ")
-        roads.append(new_record)
+    if os.path.isfile(filename):
+        absolute_path = filename
     else:
-        break
+        absolute_path = os.getcwd() + filename
 
-for i in range(len(roads)):
-    # noinspection PyTypeChecker
-    roads[i][2] = int(roads[i][2])
-    # noinspection PyTypeChecker
-    tot += roads[i][2] ** 2
+    with open(absolute_path, "r") as file:
+        starting_point = file.readline().strip()
+        ending_point = file.readline().strip()
+        headers.append(starting_point)
+        for line in file:
+            road = stripline(line)
+            roads.append(road)
+            if road[1] not in headers:
+                headers.append(road[1])
+            tot += road[2] ** 2
 
-for i in roads:
-    if i[0] not in headers:
-        headers.append(i[0])
+    queue.append(starting_point)
 
-if ending_point not in headers:
-    headers.append(ending_point)
-queue.append(starting_point)
+    first_row = [iteration, queue, [0, False]]
+    first_row.extend([[tot, False] for _ in range(len(headers) - 3)])
+    table.append(first_row)
 
-first_row = [iteration, queue, [0, False]]
-first_row.extend([[tot, False] for _ in range(len(headers) - 3)])
-table.append(first_row)
+    recent = first_row[:]
+    previous = [starting_point]
 
-recent = first_row[:]
-previous = [starting_point]
-while len(queue) > 0:
-    new_row = recent[:]
-    iteration += 1
-    new_row[0] = iteration
-    out = queue[0]
-    out_index = headers.index(out)
-    queue = queue[1:]
+    while len(queue) > 0:
+        new_row = recent[:]
+        iteration += 1
+        new_row[0] = iteration
+        out = queue[0]
+        out_index = headers.index(out)
+        queue = queue[1:]
 
-    for i in roads:
-        if i[0] == out and i[1] not in queue:
-            if recent[out_index][0] + i[2] < recent[headers.index(i[1])][0]:
-                if i[1] in previous:
-                    queue.insert(0, i[1])
-                else:
-                    previous.append(i[0])
-                    queue.append(i[1])
-    new_row[1] = queue
+        for i in roads:
+            if i[0] == out and i[1] not in queue:
+                if recent[out_index][0] + i[2] < recent[headers.index(i[1])][0]:
+                    if i[1] in previous:
+                        queue.insert(0, i[1])
+                    else:
+                        previous.append(i[0])
+                        queue.append(i[1])
+        new_row[1] = queue
 
-    for i in range(2, len(headers)):
-        for track in roads:
-            if track[0] == out and track[1] == headers[i]:
-                if recent[out_index][0] + track[2] < new_row[i][0]:
-                    new_row[i] = [recent[out_index][0] + track[2], out]
+        for i in range(2, len(headers)):
+            for track in roads:
+                if track[0] == out and track[1] == headers[i]:
+                    if recent[out_index][0] + track[2] < new_row[i][0]:
+                        new_row[i] = [recent[out_index][0] + track[2], out]
 
-    recent = new_row[:]
-    table.append(new_row)
+        recent = new_row[:]
+        table.append(new_row)
 
-answer = ending_point
-while True:
-    x = answer[0]
-    if type(recent[headers.index(x)][1]) == str:
-        path = recent[headers.index(x)][1]
-        answer = path + " -> " + answer
-    else:
-        break
+    answer = ending_point
+    while True:
+        x = answer[0]
+        if type(recent[headers.index(x)][1]) == str:
+            path = recent[headers.index(x)][1]
+            answer = path + " -> " + answer
+        else:
+            break
 
-print()
-print(tabulate(table, headers=headers))
-print()
-print("Odpowiedz:")
-print("\tdlugosc trasy:", recent[headers.index(ending_point)][0])
-print("\tkolejnosc:", answer)
+    print()
+    print(tabulate(table, headers=headers))
+    print()
+    print("Answer:")
+    print("\tTrack length:", recent[headers.index(ending_point)][0])
+    print("\tOrder:", answer)
+    print()
+
+
+def stripline(line):
+    strip = line.split()
+    return (strip[0], strip[1], int(strip[2]))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Pathfinding BMEP algorithm to find shortest path from starting node "
+                                                 "to ending node in directed graph")
+    parser.add_argument(
+        "-f", "--file",
+        help=".txt file with description of graph in given pattern: first line - starting point, "
+             "second line - ending point, following lines - roads between nodes "
+             "StartNode_EndNode_Distance (example: 'a b 5')",
+        required=True
+    )
+    args = parser.parse_args()
+    try:
+        main(args.file)
+    except FileNotFoundError:
+        print("No such file or directory")
